dce425af55638fa34543780ac2382895a931f1c9
maxrbarltrop <barl5390@mylaurier.ca>
2020-04-01 19:32:42 -0400

    Made the quote page

---
+++ src/assets/testdata/sample-quotes.js
@@ -0,0 +1,67 @@
+var testquotes =
+    [
+        {
+            name: "First Last",
+            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor elit lacus, non gravida lacus ultrices id",
+            term: "Sprallinter 2034"
+        },
+        {
+            name: "First Last",
+            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor elit lacus, non gravida lacus ultrices id",
+            term: "Sprallinter 2034"
+        },
+        {
+            name: "First Last",
+            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor elit lacus, non gravida lacus ultrices id",
+            term: "Sprallinter 2034"
+        },
+        {
+            name: "First Last",
+            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor elit lacus, non gravida lacus ultrices id",
+            term: "Sprallinter 2034"
+        },
+        {
+            name: "First Last",
+            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor elit lacus, non gravida lacus ultrices id",
+            term: "Sprallinter 2034"
+        },
+        {
+            name: "First Last",
+            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor elit lacus, non gravida lacus ultrices id",
+            term: "Sprallinter 2034"
+        },
+        {
+            name: "First Last",
+            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor elit lacus, non gravida lacus ultrices id",
+            term: "Sprallinter 2034"
+        },
+        {
+            name: "First Last",
+            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor elit lacus, non gravida lacus ultrices id",
+            term: "Sprallinter 2034"
+        },
+        {
+            name: "First Last",
+            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor elit lacus, non gravida lacus ultrices id",
+            term: "Sprallinter 2034"
+        },
+        {
+            name: "First Last",
+            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor elit lacus, non gravida lacus ultrices id",
+            term: "Sprallinter 2034"
+        },
+        {
+            name: "First Last",
+            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor elit lacus, non gravida lacus ultrices id",
+            term: "Sprallinter 2034"
+        },
+
+        {
+            name: "First Last",
+            quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor elit lacus, non gravida lacus ultrices id",
+            term: "Sprallinter 2034"
+        }
+
+    ]
+
+export default testquotes;
