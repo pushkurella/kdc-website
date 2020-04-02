5b3281808bc3f7d836a959b7ddd306980b9ff39e
maxrbarltrop <barl5390@mylaurier.ca>
2020-04-02 10:58:29 -0400

    Add button

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/App.css';
 import Nav from './Nav';
 import Footer from './Footer';
+import Quotes from './Quotes';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import history from '../history';
 import Contact from './Contact';
