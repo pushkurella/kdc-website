d800c2c3eaa092dfc560d4362a83ff3fa7b71ff9
Arshia Mathur <arshiamathur@Arshias-MacBook-Pro.local>
2020-04-06 11:41:37 -0400

    #comment added headers for Managers and Leads and added functionality for Alumni page button

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,98 +1,107 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react'
-import temp from '../images/Temp.png'
-import '../styles/Teams.css'
+import { Grid, Image } from 'semantic-ui-react';
+import temp from '../images/Temp.png';
+import '../styles/Teams.css';
+import history from '../history';
+
 
 //Temporary data
 const info = [{
-  img:temp,
+  img: temp,
   name: 'Ron Moeibrook',
   role: 'Senior Manager'
 },
 {
-  img:temp,
+  img: temp,
   name: 'Jason Murray',
   role: 'Senior Systems Development Consultant'
 },
 {
-  img:temp,
+  img: temp,
   name: 'Brenton Fairless',
   role: 'Tooling and Practice Team'
 },
 {
-  img:temp,
+  img: temp,
   name: 'Amy Barrett',
   role: 'Team Lead'
 },
 {
-  img:temp,
+  img: temp,
   name: 'Danielle kacheff',
   role: 'Team Lead'
 },
 {
-  img:temp,
+  img: temp,
   name: 'Vikas Vattikonda',
   role: 'Lead Developer'
 },
 {
-  img:temp,
+  img: temp,
   name: 'Pushpak Kurella',
   role: 'Lead Developer'
 },
 {
-  img:temp,
+  img: temp,
   name: 'Pallishree Pallavi',
   role: 'Team Lead'
 }]
 
 
-  class Teams extends React.Component{
-
-    constructor(props){//first thing that gets called
-      super(props);
-
-      this.state = {managers: info.slice(0,2), leads: info.slice(2,9)};
-    }
-    render(){
-      return (
-        <div>
-          <div className="center">
-          <h1 >Our Teams</h1>
-          <p aligntext="center">The Kitchener Development Centre has a variety 
-            of team members who all work toward one central
-             goal of providing the company with the highest 
-             quality products and providing each other with 
-             the greatest work experience possible.</p>
-          <button className="button" >Meet our Alumni</button>
-          </div>
-             <Grid columns={2} centered className="managers">
-               <Grid.Row>
-               {this.state.managers.map((value, index) => {
-                 return (<Grid.Column key = {index} className="Columns">  
-                   <div className = 'center-image'> 
-                   <Image ui centered image src = {value.img} alt="Not displayed"/>
-                     </div> 
-                   <div className="center employee-name">{value.name}</div>
-                   <div className="center employee-role">{value.role}</div>
-                   </Grid.Column>);
-               })}
-               </Grid.Row>
-               </Grid> 
 
 
-              <Grid columns={3}  className="leads">
-              {this.state.leads.map((value, index) => {
-                 return (<Grid.Column key = {index} className="Columns">
-                   <div className = 'center-image' ><Image ui centered image src = {value.img}/></div> 
-                   <div className="employee-name">{value.name}</div>
-                   <div className="employee-role">{value.role}</div>
-                   </Grid.Column>);
-               })}
-                
-              </Grid>
-        </div>
-      );
-    }
+class Teams extends React.Component {
+
+  constructor(props) {//first thing that gets called
+    super(props);
+
+    this.state = { managers: info.slice(0, 2), leads: info.slice(2, 9) };
   }
-  
-  export default Teams;
+  _linkTo = (str) => {
+    history.push(str);
+    window.location.reload();
+  }
+  render() {
+    return (
+      <div>
+        <div className="center">
+          <h1 className="title">Our Teams</h1>
+          <p aligntext="center">The Kitchener Development Centre has a variety
+          of team members who all work toward one central
+          goal of providing the company with the highest
+          quality products and providing each other with
+             the greatest work experience possible.</p>
+          <button className="button" onClick={()=>{this._linkTo("/leaveyourmark")}}>Meet our Alumni</button>
+        </div>
+
+        <h1 className="headers">Senior Members</h1>
+        <Grid columns={2} centered className="managers">
+            {this.state.managers.map((value, index) => {
+              return (<Grid.Column key={index} className="Columns">
+                <div className='center-image'>
+                  <Image ui centered image src={value.img} alt="Not displayed" />
+                </div>
+                <div className="center employee-name">{value.name}</div>
+                <div className="center employee-role">{value.role}</div>
+              </Grid.Column>);
+            })}
+        </Grid>
+
+        <h1 className="headers">Our Leads</h1>
+        <Grid columns={3} className="leads">
+          {this.state.leads.map((value, index) => {
+            return (<Grid.Column key={index} className="Columns">
+              <div className='center-image'>
+                <Image ui centered image src={value.img} />
+                </div>
+              <div className="employee-name">{value.name}</div>
+              <div className="employee-role">{value.role}</div>
+            </Grid.Column>);
+          })}
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Teams;
